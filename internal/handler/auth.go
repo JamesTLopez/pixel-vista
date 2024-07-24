d7a6a66e4e5f507bcdf33bf3782d18ebabe2eaa2
James Lopez <James.Theo.Lopez@gmail.com>
2024-07-24 16:30:00 -0400

    error label

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"pixelvista/view/pages/auth"
 
+	"github.com/a-h/templ"
 	"github.com/nedpals/supabase-go"
 )
+
+func renderComponent(w http.ResponseWriter, r *http.Request, component templ.Component) error {
+	return component.Render(r.Context(), w)
+}
 
 func HandlerSigninIndex(w http.ResponseWriter, r *http.Request) error {
 	return auth.Signin().Render(r.Context(), w)
@@ -18,7 +22,7 @@ func LoginCreate(w http.ResponseWriter, r *http.Request) error {
 		Password: r.FormValue("password"),
 	}
 
-	fmt.Println(credentials)
-
-	return nil
+	return renderComponent(w, r, auth.LoginForm(credentials, auth.LoginErrors{
+		InvalidCred: "Invalid credentials, please try again",
+	}))
 }
