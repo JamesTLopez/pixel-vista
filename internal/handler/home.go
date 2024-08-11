28b90f1066c08ab47b6914369f33d72bc44cf335
James Lopez <James.Theo.Lopez@gmail.com>
2024-08-11 17:34:42 -0400

    start removal of password auth

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -7,17 +7,5 @@ import (
 
 func HandleHomeIndex(w http.ResponseWriter, r *http.Request) error {
 
-	// user := internal.GetAuthenticatedUser(r)
-	// account := types.Account{
-	// 	UserID:   user.ID,
-	// 	Username: "foo",
-	// }
-
-	// if err := db.CreateAccount(account); err != nil {
-	// 	return err
-	// }
-
-	// fmt.Printf("%+v\n", account)
-
 	return home.Index().Render(r.Context(), w)
 }
