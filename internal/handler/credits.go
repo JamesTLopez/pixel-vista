3e413d607738e0e4c3ef8e75babe3843c782fd20
James Lopez <James.Theo.Lopez@gmail.com>
2024-09-12 14:18:45 -0400

    update checkout endpoint

--- internal/handler/credits.go
+++ internal/handler/credits.go
@@ -6,6 +6,7 @@ import (
 	"pixelvista/view/pages/credits"
 
 	"github.com/stripe/stripe-go/v79"
+	"github.com/stripe/stripe-go/v79/checkout/session"
 )
 
 func HandleCreditsIndex(w http.ResponseWriter, r *http.Request) error {
@@ -13,7 +14,24 @@ func HandleCreditsIndex(w http.ResponseWriter, r *http.Request) error {
 	return renderComponent(w, r, credits.CreditsIndex())
 }
 
-func StripeCallbackSuccess(w http.ResponseWriter, r *http.Request) error {
+func StripeCheckout(w http.ResponseWriter, r *http.Request) error {
 	stripe.Key = os.Getenv("STRIPE_API_KEY")
+	checkoutParams := &stripe.CheckoutSessionParams{
+		SuccessURL: stripe.String(""),
+		CancelURL:  stripe.String(""),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				Price:    stripe.String("__"),
+				Quantity: stripe.Int64(1),
+			},
+		},
+	}
+
+	s, err := session.New(checkoutParams)
+
+	if err != nil {
+		return err
+	}
+	http.Redirect(w, r, s.URL, http.StatusSeeOther)
 	return nil
 }
